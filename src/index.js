a14ee4cf0f4e400300e158f6378173f0d3275e76
arihito <arihito.m@gmail.com>
2017-02-05 20:04:58 +0900

    builds output

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var $ = require('jquery');
+
+$('body').html('Hello');
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // 読み込むフォルダ
+  entry: './src',
+  // 出力するパスとファイル名
+  output: {
+    path: 'builds',
+    filename: 'bundle.js',
+  }
+}
